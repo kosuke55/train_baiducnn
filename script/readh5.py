7e147961a472bec134aaac3893cfd75db72a2421
kosuke55 <kosuke.tnp@gmail.com>
2019-11-23 18:04:41 +0900

    update readh5

--- script/readh5.py
+++ script/readh5.py
@@ -4,64 +4,50 @@ import sys
 sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import cv2
 
-# infh = h5py.File('nusc_baidu.h5', 'r')
-infh = h5py.File('oneframe_nusc_baidu_confidence.h5', 'r')
-# infh = h5py.File('test_oneframe_nusc_baidu_confidence.h5', 'r')
-# infh = h5py.File('nusc_baidu_confidence.h5', 'r')
+infh = h5py.File('100_nusc_baidu_confidence.h5', 'r')
 infh.keys()
-in_feature = infh['data'].value
-# print(in_feature.shape)
-in_feature = np.transpose(
-    in_feature, (0, 3, 2, 1))  # NxCxHxW -> NxWxHxC
 
-# print(np.count_nonzero(in_feature))
-# in_image = in_feature.reshape(640, 640, 8)
-# print(in_image.shape)
-# print(in_image[:, :, 0][in_image[:, :, 0] > 0.5])
-# print(np.count_nonzero(in_image[:, :, 7]))
-
-out_feature = infh['output'].value
-print(out_feature.shape)  # NxCxHxW
+frame_id = 20
 
 loss_weight = infh['loss_weight'].value
-# print(loss_weight.shape)  # NxCxHxW
-# print(out_feature[0, :, 0, 0])
-
-# out_feature = out_feature[20]
-print(out_feature.shape)  # NxCxHxW
-
-# out_feature = np.transpose(
-#     out_feature, (2, 1, 0))  # NxCxHxW -> NxWxHxC
-######
-out_feature = np.transpose(
-    out_feature, (0, 3, 2, 1))  # NxCxHxW -> NxWxHxC
+print("loss_weight" + str(loss_weight.shape))  # NxCxHxW
 loss_weight = np.transpose(
     loss_weight, (0, 3, 2, 1))  # NxCxHxW -> NxWxHxC
-print(out_feature.shape)  # NxCxHxW
-# # print(out_feature.shape)
-# # print(np.count_nonzero(out_feature))
-out_image = out_feature.reshape(640, 640) * 255
-# # print(np.count_nonzero(out_image))
-# # print(out_image.shape)
+loss_weight = loss_weight[frame_id]
 loss_weight[np.where(loss_weight != 1)] = 0
 loss_weight = loss_weight.reshape(640, 640) * 255
+loss_weight = loss_weight.astype(np.uint8)
+cv2.imwrite("image/loss_weight.png", loss_weight)
 
-# lw_image *= 255
-# print(lw_image.shape)
+out_feature = infh['output'].value
+print("out_feature" + str(out_feature.shape))  # NxCxHxW
+out_feature = np.transpose(
+    out_feature, (0, 3, 2, 1))  # NxCxHxW -> NxWxHxC
+out_feature = out_feature[frame_id]
+out_image = out_feature.reshape(640, 640) * 255
+out_image = out_image.astype(np.uint8)
+cv2.imwrite("image/confidence_out.png", out_image)
 
-cv2.imwrite("loss_weight.png", loss_weight)
-# cv2.imwrite("confidence_out.png", out_image)
-# # in_image = in_image[:, :, 7] * 255
-for i in range(8):
+in_feature = infh['data'].value
+print("in_feature" + str(in_feature.shape))  # NxCxHxW
+in_feature = np.transpose(
+    in_feature, (0, 3, 2, 1))  # NxCxHxW -> NxWxHxC
+in_feature = in_feature[frame_id]
+
+vis_all_in_feature = False
+if(vis_all_in_feature):
+    for i in range(8):
+        in_image = in_feature.reshape(640, 640, 8)
+        in_image = in_image[:, :, i]
+        in_image[np.where(in_image != 0)] = 255
+        in_image = in_image.astype(np.uint8)
+        cv2.imwrite("image/confidence_in_{}.png".format(i), in_image)
+else:
     in_image = in_feature.reshape(640, 640, 8)
-    in_image = in_image[:, :, i]
+    in_image = in_image[:, :, 0]
     in_image[np.where(in_image != 0)] = 255
-    cv2.imwrite("confidence_in_{}.png".format(i), in_image)
+    in_image = in_image.astype(np.uint8)
+    cv2.imwrite("image/confidence_in_{}.png".format(0), in_image)
 
 
-# cv2.namedWindow('window')
-# cv2.imshow('window', out_image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-cv2.imwrite("confidence_out.png", out_image)
 infh.close()
