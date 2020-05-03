49862d2866ce6c37eaca898bec0b9a29aeaf7247
Your Name <kosuke.tnp@gmail.com>
2020-05-03 23:22:44 +0900

    Fix label annotation

--- scripts/create_dataset/create_dataset_from_nusc.py
+++ scripts/create_dataset/create_dataset_from_nusc.py
@@ -139,10 +139,10 @@ def create_dataset(dataroot, save_dir, width=672, height=672, grid_range=70.,
                             out_feature[i, j, 2] = instance_pt[1]
                             out_feature[i, j, 3] = 1.  # confidence_pt
                             # out_feature[i, j, 4] = label  # classify_pt
-                            if i - label_half_length / 2 >= 0 and \
-                               i + label_half_length / 2 < width and \
-                               j - label_half_length / 2 >= 0 and \
-                               j + label_half_length / 2 < height:
+                            if i - label_half_length >= 0 and \
+                               i + label_half_length < width and \
+                               j - label_half_length >= 0 and \
+                               j + label_half_length < height:
                                 out_feature[
                                     i - label_half_length:i + label_half_length,
                                     j - label_half_length:j + label_half_length,
