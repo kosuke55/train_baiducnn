ac5d97bf645f0f10a17d66baf44c3fbd2a16524b
Your Name <kosuke.tnp@gmail.com>
2020-05-11 19:34:01 +0900

    Update instance_pt and yaw(tmp)

--- scripts/create_dataset/create_dataset_from_nusc.py
+++ scripts/create_dataset/create_dataset_from_nusc.py
@@ -52,148 +52,182 @@ def create_dataset(dataroot, save_dir, width=672, height=672, grid_range=70.,
         first_sample_token = my_scene['first_sample_token']
         token = first_sample_token
 
-        try:
-            while(token != ''):
-                print('--- {} '.format(data_id) + token + ' ---')
-                my_sample = nusc.get('sample', token)
-                sd_record = nusc.get(
-                    'sample_data', my_sample['data'][ref_chan])
-                sample_rec = nusc.get('sample', sd_record['sample_token'])
-                chan = sd_record['channel']
+        # try:
+        while(token != ''):
+            print('--- {} '.format(data_id) + token + ' ---')
+            my_sample = nusc.get('sample', token)
+            sd_record = nusc.get(
+                'sample_data', my_sample['data'][ref_chan])
+            sample_rec = nusc.get('sample', sd_record['sample_token'])
+            chan = sd_record['channel']
 
-                pc, times = LidarPointCloud.from_file_multisweep(
-                    nusc, sample_rec, chan, ref_chan, nsweeps=1)
-                _, boxes, _ = nusc.get_sample_data(
-                    sd_record['token'], box_vis_level=0)
+            pc, times = LidarPointCloud.from_file_multisweep(
+                nusc, sample_rec, chan, ref_chan, nsweeps=1)
+            _, boxes, _ = nusc.get_sample_data(
+                sd_record['token'], box_vis_level=0)
 
-                out_feature = np.zeros((size, size, 7), dtype=np.float32)
-                for box_idx, box in enumerate(boxes):
-                    label = 0
-                    if box.name.split('.')[0] == 'vehicle':
-                        if box.name.split('.')[1] == 'car':
-                            label = 1
-                        elif box.name.split('.')[1] == 'bus':
-                            label = 1
-                        elif box.name.split('.')[1] == 'truck':
-                            label = 1
-                        elif box.name.split('.')[1] == 'construction':
-                            label = 1
-                        elif box.name.split('.')[1] == 'emergency':
-                            label = 1
-                        elif box.name.split('.')[1] == 'trailer':
-                            label = 1
-                        elif box.name.split('.')[1] == 'bicycle':
-                            label = 2
-                        elif box.name.split('.')[1] == 'motorcycle':
-                            label = 2
-                    elif box.name.split('.')[0] == 'human':
-                        label = 3
-                    # elif box.name.split('.')[0] == 'movable_object':
-                    #     label = 1
-                    # elif box.name.split('.')[0] == 'static_object':
-                    #     label = 1
-                    else:
-                        continue
-
-                    height_pt = np.linalg.norm(
-                        box.corners().T[0] - box.corners().T[3])
-                    corners2d = box.corners()[:2, :]
-                    box2d = corners2d.T[[2, 3, 7, 6]]
-
-                    box2d_center = box2d.mean(axis=0)
-                    generate_out_feature(width, height, size, grid_centers,
-                                         box2d, box2d_center, height_pt,
-                                         label, label_half_length, out_feature)
-
-                out_feature = out_feature.astype(np.float16)
-                feature_generator = fg.Feature_generator(
-                    grid_range, width, height,
-                    use_constant_feature, use_intensity_feature)
-                feature_generator.generate(pc.points.T)
-                in_feature = feature_generator.feature
-                if use_constant_feature and use_intensity_feature:
-                    in_feature = in_feature.reshape(size, size, 8)
-                elif use_constant_feature or use_intensity_feature:
-                    in_feature = in_feature.reshape(size, size, 6)
+            out_feature = np.zeros((size, size, 7), dtype=np.float32)
+            for box_idx, box in enumerate(boxes):
+                label = 0
+                if box.name.split('.')[0] == 'vehicle':
+                    if box.name.split('.')[1] == 'car':
+                        label = 1
+                    elif box.name.split('.')[1] == 'bus':
+                        label = 1
+                    elif box.name.split('.')[1] == 'truck':
+                        label = 1
+                    elif box.name.split('.')[1] == 'construction':
+                        label = 1
+                    elif box.name.split('.')[1] == 'emergency':
+                        label = 1
+                    elif box.name.split('.')[1] == 'trailer':
+                        label = 1
+                    elif box.name.split('.')[1] == 'bicycle':
+                        label = 2
+                    elif box.name.split('.')[1] == 'motorcycle':
+                        label = 2
+                elif box.name.split('.')[0] == 'human':
+                    label = 3
+                # elif box.name.split('.')[0] == 'movable_object':
+                #     label = 1
+                # elif box.name.split('.')[0] == 'static_object':
+                #     label = 1
                 else:
-                    in_feature = in_feature.reshape(size, size, 4)
+                    continue
 
-                # instance_pt is flipped due to flip
-                out_feature = np.flip(np.flip(out_feature, axis=0), axis=1)
-                out_feature[:, :, 1:3] *= -1
+                height_pt = np.linalg.norm(
+                    box.corners().T[0] - box.corners().T[3])
+                corners2d = box.corners()[:2, :]
+                box2d = corners2d.T[[2, 3, 7, 6]]
+                box2d_center = box2d.mean(axis=0)
+                yaw, pitch, roll = box.orientation.yaw_pitch_roll
+                print(yaw)
+                generate_out_feature(width, height, size, grid_centers,
+                                     box2d, box2d_center, height_pt,
+                                     label, label_half_length, yaw,
+                                     out_feature)
+                # import pdb; pdb.set_trace()
 
-                np.save(os.path.join(
-                    save_dir, 'in_feature/{:05}'.format(data_id)), in_feature)
-                np.save(
-                    os.path.join(
-                        save_dir,
-                        'out_feature/{:05}'.format(data_id)),
-                    out_feature)
+            out_feature = out_feature.astype(np.float16)
+            feature_generator = fg.Feature_generator(
+                grid_range, width, height,
+                use_constant_feature, use_intensity_feature)
+            feature_generator.generate(pc.points.T)
+            in_feature = feature_generator.feature
+            if use_constant_feature and use_intensity_feature:
+                in_feature = in_feature.reshape(size, size, 8)
+            elif use_constant_feature or use_intensity_feature:
+                in_feature = in_feature.reshape(size, size, 6)
+            else:
+                in_feature = in_feature.reshape(size, size, 4)
 
-                token = my_sample['next']
-                data_id += 1
-                if data_id == end_id:
-                    return
-        except KeyboardInterrupt:
-            return
-        except BaseException:
-            print('skipped')
-            continue
+            # instance_pt is flipped due to flip
+            # out_feature = np.flip(np.flip(out_feature, axis=0), axis=1)
+            # out_feature[:, :, 1:3] *= -1
+
+            np.save(os.path.join(
+                save_dir, 'in_feature/{:05}'.format(data_id)),
+                in_feature)
+            np.save(os.path.join(
+                save_dir, 'out_feature/{:05}'.format(data_id)),
+                out_feature)
+
+            token = my_sample['next']
+            data_id += 1
+            if data_id == end_id:
+                return
+        # except KeyboardInterrupt:
+        #     return
+        # except BaseException:
+        #     print('skipped')
+        #     continue
 
 
 @numba.jit(nopython=True)
 def generate_out_feature(
         width, height, size, grid_centers, box2d, box2d_center,
-        height_pt, label, label_half_length, out_feature):
+        height_pt, label, label_half_length, yaw, out_feature):
     box2d_left = box2d[:, 0].min()
     box2d_right = box2d[:, 0].max()
     box2d_top = box2d[:, 1].max()
     box2d_bottom = box2d[:, 1].min()
 
-    search_area_left_idx = np.abs(
-        grid_centers - box2d_left).argmin() - 1
-    search_area_right_idx = np.abs(
-        grid_centers - box2d_right).argmin() + 1
-    search_area_bottom_idx = np.abs(
-        grid_centers - box2d_bottom).argmin() - 1
-    search_area_top_idx = np.abs(
-        grid_centers - box2d_top).argmin() + 1
+    def F2I(val, orig, scale):
+        return int(np.floor((orig - val) * scale))
 
-    for i in range(search_area_left_idx, search_area_right_idx):
+    def Pixel2pc(in_pixel, in_size, out_range):
+        res = 2.0 * out_range / in_size
+        return out_range - (in_pixel + 0.5) * res
+    # search_area_left_idx = np.abs(
+    #     grid_centers - box2d_left).argmin() - 1
+    # search_area_right_idx = np.abs(
+    #     grid_centers - box2d_right).argmin() + 1
+    # search_area_bottom_idx = np.abs(
+    #     grid_centers - box2d_bottom).argmin() - 1
+    # search_area_top_idx = np.abs(
+    #     grid_centers - box2d_top).argmin() + 1
+
+    inv_res = 0.5 * width / 70.
+    search_area_left_idx = F2I(box2d_left, 70, inv_res)
+    search_area_right_idx = F2I(box2d_right, 70, inv_res)
+    search_area_top_idx = F2I(box2d_top, 70, inv_res)
+    search_area_bottom_idx = F2I(box2d_bottom, 70, inv_res)
+
+    # for i in range(
+    #         search_area_left_idx, search_area_right_idx):
+    #     for j in range(
+    #             search_area_bottom_idx, search_area_top_idx):
+
+    for i in range(
+            search_area_right_idx - 1, search_area_left_idx + 1):
         for j in range(
-                search_area_bottom_idx, search_area_top_idx):
-            grid_center = np.array(
-                [grid_centers[i], grid_centers[j]])
+                search_area_top_idx - 1, search_area_bottom_idx + 1):
+
+            # grid_center = np.array(
+            #     [grid_centers[i], grid_centers[j]])
+            grid_center_x = Pixel2pc(i, float(height), 70)
+            grid_center_y = Pixel2pc(j, float(width), 70)
 
             p1 = box2d[0]
             p_x = box2d[1]
             p_y = box2d[3]
             pi = p_x - p1
             pj = p_y - p1
-            v = grid_center - p1
+            v = np.array([grid_center_x, grid_center_y]) - p1
             iv = np.dot(pi, v)
             jv = np.dot(pj, v)
             mask_x = np.logical_and(0 <= iv, iv <= np.dot(pi, pi))
             mask_y = np.logical_and(0 <= jv, jv <= np.dot(pj, pj))
             mask = np.logical_and(mask_x, mask_y)
 
+            # if mask:
+            #     out_feature[i, j, 0] = 1.  # category_pt
+            #     instance_pt = box2d_center - grid_center
+            #     out_feature[i, j, 1] = instance_pt[0]
+            #     out_feature[i, j, 2] = instance_pt[1]
+
+            #     out_feature[i, j, 3] = 1.  # confidence_pt
+            #     out_feature[i, j, 4] = label  # classify_pt
+            #     # if i - label_half_length >= 0 and \
+            #     #    i + label_half_length < width and \
+            #     #    j - label_half_length >= 0 and \
+            #     #    j + label_half_length < height:
+            #     #     out_feature[
+            #     #         i - label_half_length:i + label_half_length,
+            #     #         j - label_half_length:j + label_half_length,
+            #     #         4] = label  # classify_pt
+            #     out_feature[i, j, 5] = yaw  # heading_pt (unused)
+            #     out_feature[i, j, 6] = height_pt  # height_pt
+
             if mask:
                 out_feature[i, j, 0] = 1.  # category_pt
-                instance_pt = box2d_center - grid_center
-                out_feature[i, j, 1] = instance_pt[0]
-                out_feature[i, j, 2] = instance_pt[1]
+                out_feature[i, j, 1] = (box2d_center[0] - grid_center_x) * -1
+                out_feature[i, j, 2] = (box2d_center[1] - grid_center_y) * -1
                 out_feature[i, j, 3] = 1.  # confidence_pt
                 out_feature[i, j, 4] = label  # classify_pt
-                # if i - label_half_length >= 0 and \
-                #    i + label_half_length < width and \
-                #    j - label_half_length >= 0 and \
-                #    j + label_half_length < height:
-                #     out_feature[
-                #         i - label_half_length:i + label_half_length,
-                #         j - label_half_length:j + label_half_length,
-                #         4] = label  # classify_pt
-                out_feature[i, j, 5] = 0.  # heading_pt (unused)
+                out_feature[i, j, 5] = yaw  # heading_pt (unused)
                 out_feature[i, j, 6] = height_pt  # height_pt
+
     return out_feature
 
 
--- scripts/utils/visualize_utils.py
+++ scripts/utils/visualize_utils.py
@@ -1,21 +1,97 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import os
+from __future__ import absolute_import
+from __future__ import division
 
+import os
+import os.path as osp
+import sys
+for path in sys.path:
+    if 'opt/ros/' in path:
+        print('sys.path.remove({})'.format(path))
+        sys.path.remove(path)
+
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torchviz import make_dot
 
-from BCNN import BCNN
+sys.path.append(osp.dirname(osp.dirname(osp.abspath(__file__))))
+from pytorch.BCNN import BCNN
+
+
+def viz_in_feature(
+        data_path, idx=0, width=640, height=640, grid_range=60.,
+        draw_instance_pt=False, draw_heading_pt=False):
+
+    data_name = os.listdir(os.path.join(data_path, 'in_feature'))[idx]
+    in_feature = np.load(os.path.join(data_path, 'in_feature/', data_name))
+
+    non_empty_channle = 7
+    non_empty = in_feature[..., non_empty_channle].astype(np.uint8) * 255
+    cv2.imwrite("non_empty.png", non_empty)
+    # return
+
+    if width == height:
+        size = width
+    else:
+        raise Exception(
+            'Currently only supported if width and height are equal')
+
+    grid_length = 2. * grid_range / size
+    grid_ticks = np.arange(-grid_range, grid_range + grid_length, grid_length)
+
+    grid_centers = (grid_ticks + grid_length / 2)[:len(grid_ticks) - 1]
+
+    # def fill_grid(i, j, color):
+    #     grid_center = np.array([grid_centers[i], grid_centers[j]])
+    #     fill_area = np.array([[(grid_center[0] - grid_length / 2),
+    #                            (grid_center[0] + grid_length / 2),
+    #                            (grid_center[0] + grid_length / 2),
+    #                            (grid_center[0] - grid_length / 2)],
+    #                           [(grid_center[1] + grid_length / 2),
+    #                            (grid_center[1] + grid_length / 2),
+    #                            (grid_center[1] - grid_length / 2),
+    #                            (grid_center[1] - grid_length / 2)]])
+    #     plt.fill(fill_area[0], fill_area[1], color=color, alpha=0.1)
+
+    def fill_grid(i, j, color):
+        grid_center = np.array([grid_centers[j], -grid_centers[i]])
+        # grid_center = np.array([-grid_centers[i], grid_centers[j]])
+        # grid_center = np.array([grid_centers[i], grid_centers[j]])
+        fill_area = np.array([[(grid_center[0] - grid_length / 2),
+                               (grid_center[0] + grid_length / 2),
+                               (grid_center[0] + grid_length / 2),
+                               (grid_center[0] - grid_length / 2)],
+                              [(grid_center[1] + grid_length / 2),
+                               (grid_center[1] + grid_length / 2),
+                               (grid_center[1] - grid_length / 2),
+                               (grid_center[1] - grid_length / 2)]])
+        plt.fill(fill_area[0], fill_area[1], color=color, alpha=0.1)
+
+    for i in range(height):
+        for j in range(width):
+            if in_feature[i, j, non_empty_channle] == 1:
+                fill_grid(i, j, 'r')
+
+    # for i in range(width):
+    #     for j in range(height):
+    #         if in_feature[i, j, non_empty_channle] == 1:
+    #             fill_grid(i, j, 'r')
+
+    plt.show()
 
 
 def viz_out_feature(
-        data_path, idx=0, width=640, height=640, grid_range=60.):
+        data_path, idx=0, width=640, height=640, grid_range=60.,
+        draw_instance_pt=False, draw_heading_pt=False):
 
     data_name = os.listdir(os.path.join(data_path, 'in_feature'))[idx]
+    in_feature = np.load(os.path.join(data_path, 'in_feature/', data_name))
     out_feature = np.load(os.path.join(data_path, 'out_feature/', data_name))
+    print(out_feature.shape)
 
     if width == height:
         size = width
@@ -29,7 +105,7 @@ def viz_out_feature(
     grid_centers = (grid_ticks + grid_length / 2)[:len(grid_ticks) - 1]
 
     def fill_grid(i, j, color):
-        grid_center = np.array([grid_centers[i], grid_centers[j]])
+        grid_center = np.array([grid_centers[j], -grid_centers[i]])
         fill_area = np.array([[(grid_center[0] - grid_length / 2),
                                (grid_center[0] + grid_length / 2),
                                (grid_center[0] + grid_length / 2),
@@ -41,20 +117,46 @@ def viz_out_feature(
 
         plt.fill(fill_area[0], fill_area[1], color=color, alpha=0.1)
 
-    for i in range(width):
-        for j in range(height):
-            if out_feature[i, j, 0] == 1:
-                fill_grid(i, j, 'r')
-                grid_center = np.array([grid_centers[i], grid_centers[j]])
-                plt.arrow(x=grid_center[0],
-                          y=grid_center[1],
-                          dx=out_feature[i, j, 1],
-                          dy=out_feature[i, j, 2],
-                          width=0.01,
-                          head_width=0.05,
-                          head_length=0.05,
-                          length_includes_head=True,
-                          color='k')
+    if draw_instance_pt:
+        print('draw instance pt')
+        for i in range(height):
+            for j in range(width):
+                if in_feature[i, j, 5] == 1:
+                    fill_grid(i, j, 'r')
+                if out_feature[i, j, 0] == 1:
+                    fill_grid(i, j, 'b')
+                    grid_center = np.array([grid_centers[j], -grid_centers[i]])
+                    plt.arrow(x=grid_center[0],
+                              y=grid_center[1],
+                              dx=out_feature[i, j, 2],
+                              dy=-out_feature[i, j, 1],
+                              width=0.01,
+                              head_width=0.05,
+                              head_length=0.05,
+                              length_includes_head=True,
+                              color='k')
+
+    if draw_heading_pt:
+        for i in range(height):
+            for j in range(width):
+                if in_feature[i, j, 5] == 1:
+                    fill_grid(i, j, 'r')
+                if out_feature[i, j, 0] == 1:
+                    fill_grid(i, j, 'b')
+                    grid_center = np.array([grid_centers[j], -grid_centers[i]])
+                    heading_pt = out_feature[i, j, 6]
+                    dx = -1.0 * np.cos(heading_pt)
+                    dy = 1.0 * np.sin(heading_pt)
+                    plt.arrow(x=grid_center[0],
+                              y=grid_center[1],
+                              dx=dx,
+                              dy=-dy,
+                              width=0.01,
+                              head_width=0.05,
+                              head_length=0.05,
+                              length_includes_head=True,
+                              color='k')
+
     plt.show()
 
 
@@ -70,7 +172,17 @@ def visualize_model():
 
 if __name__ == '__main__':
     viz_out_feature(
-        data_path='/media/kosuke/HD-PNFU3/nusc/feature_instance_one',
-        idx=0, width=640, height=640, grid_range=60.)
+        data_path='/media/kosuke/SANDISK/nusc/mini-6c-672-yaw',
+        idx=0, width=672, height=672, grid_range=70.,
+        draw_instance_pt=False, draw_heading_pt=True)
+
+        # draw_instance_pt=True, draw_heading_pt=False)
+
+    # viz_out_feature(
+    #     data_path='/media/kosuke/SANDISK/nusc/mini-0427',
+    #     idx=0, width=640, height=640, grid_range=60.,
+    #     draw_instance_pt=True, draw_heading_pt=False)
+
+
 
     visualize_model()
