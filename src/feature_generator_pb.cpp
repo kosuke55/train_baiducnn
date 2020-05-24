849fdc5462d11fa1eabf5555a05bdd9f5dbfbfd7
Your Name <kosuke.tnp@gmail.com>
2020-05-25 01:08:42 +0900

    Update feature_generator_pb.cpp

--- src/feature_generator_pb.cpp
+++ src/feature_generator_pb.cpp
@@ -16,19 +16,9 @@
 
 #include "feature_generator_pb.h"
 
-FeatureGenerator::FeatureGenerator(
-    int range, int width,
-    int height)
-    // const bool use_constant_feature,
-    // const bool use_intensity_feature)
-    : min_height_(-5.0),
-      max_height_(5.0),
-      range_(range),
-      width_(width),
-      height_(height){
-      // use_constant_feature_(use_constant_feature),
-      // use_intensity_feature_(use_intensity_feature) {
+FeatureGenerator::FeatureGenerator(int range, int width, int height)
 
+    : min_height_(-5.0), max_height_(5.0), range_(range), width_(width), height_(height) {
   log_table_.resize(256);
   for (size_t i = 0; i < log_table_.size(); ++i) {
     log_table_[i] = std::log1p(static_cast<float>(i));
@@ -44,17 +34,15 @@ float FeatureGenerator::logCount(int count) {
   return std::log(static_cast<float>(1 + count));
 }
 
-std::vector<float> FeatureGenerator::generate(
-    const pybind11::array_t<float> points,
-    const bool use_constant_feature, const bool use_intensity_feature) {
-
-  const auto &buff_info = points.request();
-  const auto &shape = buff_info.shape;
+std::vector<float> FeatureGenerator::generate(const pybind11::array_t<float> points, const bool use_constant_feature,
+                                              const bool use_intensity_feature) {
+  const auto& buff_info = points.request();
+  const auto& shape = buff_info.shape;
 
   int siz = height_ * width_;
   std::vector<float> in_feature;
 
-  float *max_height_data, *mean_height_data, *count_data, *top_intensity_data,
+  float *max_height_data, *direction_data, *mean_height_data, *distance_data, *count_data, *top_intensity_data,
       *mean_intensity_data, *nonempty_data;
 
   int channels;
@@ -62,13 +50,17 @@ std::vector<float> FeatureGenerator::generate(
     channels = 8;
     in_feature.resize(siz * channels);
     max_height_data = &in_feature[0];
+    direction_data = &in_feature[0] + siz * 3;
     top_intensity_data = &in_feature[0] + siz * 4;
     mean_intensity_data = &in_feature[0] + siz * 5;
+    distance_data = &in_feature[0] + siz * 6;
     nonempty_data = &in_feature[0] + siz * 7;
   } else if (use_constant_feature) {
     channels = 6;
     in_feature.resize(siz * channels);
     max_height_data = &in_feature[0];
+    direction_data = &in_feature[0] + siz * 3;
+    distance_data = &in_feature[0] + siz * 4;
     nonempty_data = &in_feature[0] + siz * 5;
   } else if (use_intensity_feature) {
     channels = 6;
@@ -84,47 +76,33 @@ std::vector<float> FeatureGenerator::generate(
     nonempty_data = &in_feature[0] + siz * 3;
   }
 
-  // float *direction_data, *distance_data;
-  // if (use_constant_feature && use_intensity_feature) {
-  //   direction_data = in_feature_ + siz * 3;
-  //   distance_data = in_feature_ + siz * 6;
-  // } else if (use_constant_feature) {
-  //   direction_data = in_feature_ + siz * 3;
-  //   distance_data = in_feature_ + siz * 4;
-  // }
-
-  // if (use_constant_feature) {
-  //   for (int row = 0; row < height_; ++row) {
-  //     for (int col = 0; col < width_; ++col) {
-  //       int idx = row * width_ + col;
-  //       float center_x = Pixel2Pc(row, height_, range_);
-  //       float center_y = Pixel2Pc(col, width_, range_);
-  //       constexpr double K_CV_PI = 3.1415926535897932384626433832795;
-  //       direction_data[idx] = static_cast<float>(
-  //           std::atan2(center_y, center_x) / (2.0 * K_CV_PI));
-  //       distance_data[idx] =
-  //           static_cast<float>(std::hypot(center_x, center_y) / 60.0 - 0.5);
-  //     }
-  //   }
-  // }
-
   mean_height_data = &in_feature[0] + siz;
   count_data = &in_feature[0] + siz * 2;
 
-  for (int i = 0; i < siz * channels; ++i) {
-    if (i < siz){
-      in_feature[i] = -0.5;
+  if (use_constant_feature) {
+    for (int row = 0; row < height_; ++row) {
+      for (int col = 0; col < width_; ++col) {
+        int idx = row * width_ + col;
+        float center_x = Pixel2Pc(row, height_, range_);
+        float center_y = Pixel2Pc(col, width_, range_);
+        constexpr double K_CV_PI = 3.1415926535897932384626433832795;
+        direction_data[idx] = static_cast<float>(std::atan2(center_y, center_x) / (2.0 * K_CV_PI));
+        distance_data[idx] = static_cast<float>(std::hypot(center_x, center_y) / 60.0 - 0.5);
+      }
     }
-    else{
+  }
+
+  for (int i = 0; i < siz * channels; ++i) {
+    if (i < siz) {
+      in_feature[i] = -0.5;
+    } else {
       in_feature[i] = 0;
     }
   }
 
   map_idx_.resize(shape[0]);
-  float inv_res_x =
-      0.5 * static_cast<float>(width_) / static_cast<float>(range_);
-  float inv_res_y =
-      0.5 * static_cast<float>(height_) / static_cast<float>(range_);
+  float inv_res_x = 0.5 * static_cast<float>(width_) / static_cast<float>(range_);
+  float inv_res_y = 0.5 * static_cast<float>(height_) / static_cast<float>(range_);
 
   for (auto i = 0; i < shape[0]; ++i) {
     if (*points.data(i, 2) <= min_height_ || *points.data(i, 2) >= max_height_) {
@@ -163,16 +141,15 @@ std::vector<float> FeatureGenerator::generate(
   }
 
   return in_feature;
-
 }
 
 namespace py = pybind11;
 PYBIND11_PLUGIN(feature_generator_pb) {
-    py::module m("feature_generator_pb", "feature_generator made by pybind11");
+  py::module m("feature_generator_pb", "feature_generator made by pybind11");
 
-    py::class_<FeatureGenerator>(m, "FeatureGenerator")
-        .def(py::init<int, int, int>())
-        .def("generate", &FeatureGenerator::generate);
+  py::class_<FeatureGenerator>(m, "FeatureGenerator")
+      .def(py::init<int, int, int>())
+      .def("generate", &FeatureGenerator::generate);
 
-    return m.ptr();
+  return m.ptr();
 }
